2eed986e1bb7af3d88bc58624b753b8196b1c087
GuuhGO <gu.guimaraesrdc9@gmail.com>
2022-03-12 01:17:05 -0300

    First commit

---
+++ src/com/dio/TesteGitCLI.java
@@ -0,0 +1,10 @@
+package com.dio;
+
+public class TesteGitCLI {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, Mundo, estou aprendendo JAVA e versionamento de código!");
+		//Este programa foi criado para me ajudar a treinar a linguagem java e o controle de ver
+	}
+
+}
